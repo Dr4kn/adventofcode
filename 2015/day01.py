aed279d9b05865d689b274adea40b530624d8ff4
Dr4kn <servicesDrkn@gmail.com>
2024-05-30 15:53:00 +0200

    2015-01 done

--- 2015/day01.py
+++ 2015/day01.py
@@ -1,3 +1,13 @@
 from aocd import get_data
 
-get_data(day=1, year=2015)
+floor = 0
+count = 0
+basement = False
+for i in get_data(day=1, year=2015):
+    floor = floor + 1 if i == '(' else floor - 1
+    count += 1
+    if floor == -1 and basement == False:
+        basement = True
+        print(count)
+print(floor)
+
