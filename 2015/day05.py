e719ed376249f88f7c0f8cf1d904a48fafc74521
Dr4kn <servicesDrkn@gmail.com>
2024-06-06 10:31:55 +0200

    2015-05 part1 done

---
+++ 2015/day05.py
@@ -0,0 +1,29 @@
+from aocd import get_data
+data = get_data(day=5, year=2015)
+
+def part1():
+    nice_string = 0
+    for line in data.splitlines(False):
+        double_letter = False
+        vowels = 0
+        if line[-1] in "aeiou":
+            vowels += 1
+        for i in range(len(line) - 1):
+            substring = line[i:i+2]
+            if substring == "ab" or substring == "cd" or substring == "pq" or substring == "xy":
+                break
+            if substring[0] == substring[1]:
+                double_letter = True
+            if line[i] in "aeiou":
+                vowels += 1
+            if i == 14 and double_letter and vowels >= 3:
+                nice_string += 1
+
+    print("part1: " + str(nice_string))
+
+def part2():
+    print("part2: ")
+
+if __name__ == "__main__":
+    part1()
+    part2()
