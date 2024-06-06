cc71774abba91181ffd196135fce284d321e994c
Dr4kn <servicesDrkn@gmail.com>
2024-06-06 10:31:32 +0200

    new template

--- day.py
+++ day.py
@@ -1,3 +1,12 @@
 from aocd import get_data
+data = get_data(day=1, year=2015)
 
-get_data(day=, year=20)
+def part1():
+    print("part1: ")
+
+def part2():
+    print("part2: ")
+
+if __name__ == "__main__":
+    part1()
+    part2()
