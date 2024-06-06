313bfe5480dee9c18efdd47bb4eff65322504ab5
Dr4kn <servicesDrkn@gmail.com>
2024-06-06 11:37:09 +0200

    2015-05 done

--- 2015/day05.py
+++ 2015/day05.py
@@ -22,7 +22,20 @@ def part1():
     print("part1: " + str(nice_string))
 
 def part2():
-    print("part2: ")
+    nice_string = 0
+    for line in data.splitlines(False):
+        space_repeat = False
+        letter_pair = False
+        for i in range(len(line) - 2):
+            if line[i] + line[i+1] in line[i+2:]:
+                letter_pair = True
+            if line[i] == line[i + 2]:
+                space_repeat = True
+            if letter_pair and space_repeat:
+                nice_string += 1
+                break
+
+    print("part2: " + str(nice_string))
 
 if __name__ == "__main__":
     part1()
